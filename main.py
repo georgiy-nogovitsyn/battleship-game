d1f8743a74fb652ba9b281f3ba2555a67aec636d
Geo <george.nogovitsin@gmail.com>
2023-04-29 02:38:08 +0300

    create attack in player class

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ comp = computer.Comp()
 player = player.Player()
 player.ship_placement()
 
-for x in range(100):
+# for x in range(100):
+#     comp.attack(player.ships, player.board.field)
+#     print(x)
+
+# for ship in player.ships:
+#     print(ship.status_update())
+# comp.board.draw(comp.ships, player.ships)
+# player.board.draw(player.ships, comp.ships)
+
+while True:
+    player.attack(comp.ships, comp.board.field)
     comp.attack(player.ships, player.board.field)
-    print(x)
-
-for ship in player.ships:
-    print(ship.status_update())
-comp.board.draw(comp.ships, player.ships)
-player.board.draw(player.ships, comp.ships)
-
+    player.board.draw(player.ships, comp.ships)
 
--- player.py
+++ player.py
@@ -39,7 +39,49 @@ class Player:
                     break
 
     def attack(self, opponent_ships, opponent_field):
-        pass
+        flag = False, False
+        while True:
+            if flag == (True, True):
+                break
+            else:
+                flag = False, False
+            while True:
+                player_input = input('Enter the x, y coordinates from 00 to 99, e.g. 37: ')
+                if player_input.isdigit() and len(player_input) == 2:
+                    # print(player_input)
+                    choice = int(player_input[0]), int(player_input[1])
+                    print(choice)
+                    break
+                else:
+                    print('Input is not valid.')
+            for ship in opponent_ships:
+                if choice in ship.coordinates:
+                    if ship.coordinates[choice] == 1:
+                        print('Ship is on fire!')
+                        ship.coordinates[choice] = 0
+                        flag = True, True
+                        break
+                    elif ship.coordinates[choice] == 0:
+                        print('You already hit that cell')
+                        flag = True, False
+                        break
+            if flag == (True, True):
+                break
+            elif flag == (True, False):
+                continue
+            else:
+                for i, row in enumerate(self.board.battlefield):
+                    if choice in row:
+                        if row[choice] == 1:
+                            print('Missed.')
+                            row[choice] = 0
+                            opponent_field[i][choice] = 0
+                            flag = True, True
+                            break
+                        elif row[choice] == 0:
+                            print('You already hit that cell')
+                            break
+
 
 # [-1][-1]     [-1][+1]
 #         0 0 0
