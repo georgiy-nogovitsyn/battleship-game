a4d7d93d87b0c5c7dce68bbb48709e033302d7b3
mireal <moveloader@yandex.ru>
2023-05-07 20:39:35 +0300

    created pygame_utils.py
    Functions for drawing basic board and getting board coordinate wwas moved to pygame_config

--- pygame battleships/pygame_config.py
+++ pygame battleships/pygame_config.py
@@ -1,6 +1,24 @@
+#Ship colors
 CLEAN_CELL = (255, 255, 255)
 DAMAGED_CELL = (128, 128, 255)
 SHIP_CELL = (128, 128, 128)
 DAMAGED_SHIP_CELL = (255, 128, 128)
 
 HIGHLIGHT_COLOR = (255,0,0)
+
+#Basic colors
+GREY = (150,150,150)
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+
+# Cell size
+CELL = 30
+
+# Settings for the game window
+WIDTH = CELL * 23 #800
+HEIGHT = CELL * 12 #500
+
+# Offsets for correct rectangle drawing
+left_border_offset = CELL
+right_border_offset = WIDTH - CELL * 11
+top_offset = CELL
