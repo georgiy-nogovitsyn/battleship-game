7931765387bf5cca1d399cb6605b0d31e220fb57
mireal <moveloader@yandex.ru>
2023-05-07 21:27:56 +0300

    enumeration function
    Added function for drawing numbers on gameboard

--- pygame battleships/pygame_config.py
+++ pygame battleships/pygame_config.py
@@ -1,22 +1,22 @@
-#Ship colors
+# Ship colors
 CLEAN_CELL = (255, 255, 255)
 DAMAGED_CELL = (128, 128, 255)
 SHIP_CELL = (128, 128, 128)
 DAMAGED_SHIP_CELL = (255, 128, 128)
 
-HIGHLIGHT_COLOR = (255,0,0)
+HIGHLIGHT_COLOR = (255, 0, 0)
 
-#Basic colors
-GREY = (150,150,150)
-BLACK = (0,0,0)
+# Basic colors
+GREY = (150, 150, 150)
+BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
 # Cell size
 CELL = 30
 
 # Settings for the game window
-WIDTH = CELL * 23 #800
-HEIGHT = CELL * 12 #500
+WIDTH = CELL * 23  # 800
+HEIGHT = CELL * 12  # 500
 
 # Offsets for correct rectangle drawing
 left_border_offset = CELL
