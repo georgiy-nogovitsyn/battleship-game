83bb54368878837566ac05cb845d164d3ff88513
Geo <george.nogovitsin@gmail.com>
2023-05-11 20:31:15 +0300

    added sprites load in the ship class

--- pygame battleships/pygame_ship_class.py
+++ pygame battleships/pygame_ship_class.py
@@ -1,11 +1,18 @@
-
+import pygame
+import assets
+from pygame_config import CELL
 class Ship:
     def __init__(self, decks):
         self.decks = decks
         self.coordinates = {}
         self.orientation = int()
         self.status = True
+        self.sprite = pygame.image.load(f'./assets/ship_{self.decks}.png')
 
     def status_update(self):
         if 1 not in self.coordinates.values():
             self.status = False
+
+abc = Ship(4)
+
+print(abc.sprite)
